221e9cb1a8d24774c5cdaa5539b81ef37c7963a6
Joseph Kligel <jkligel@gmail.com>
2020-03-27 15:47:56 -0500

    Using bash script to push all updates

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public/"));
-app.set('view engine', ejs);
+app.set("view engine", ejs);
 
 //---------------------Port Section---------------------
 const PORT = process.env.PORT || 3000
@@ -17,7 +17,14 @@ app.listen(PORT, () => {
 });
 
 //---------------------Route Section---------------------
-app.route("/").
-get((req, res) => {
-  res.send("Connected and displaying")
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
 });
