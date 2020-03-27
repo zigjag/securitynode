c3e6778b5648f0de693a7c5ce2500387309dbce1
Joseph Kligel <jkligel@gmail.com>
2020-03-27 15:00:24 -0500

    Using bash script to push all updates

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 //jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public/"));
+app.set('view engine', ejs);
+
+//---------------------Port Section---------------------
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log("Server connected on " + PORT);
+});
+
+//---------------------Route Section---------------------
+app.route("/").
+get((req, res) => {
+  res.send("Connected and displaying")
+});
