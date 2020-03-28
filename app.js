476adf49eed0309ee85f313fe5ad02e5f25570a2
Joseph Kligel <jkligel@gmail.com>
2020-03-28 13:25:45 -0500

    add gitignore files

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.use(bodyParser.urlencoded({
@@ -10,8 +12,21 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public/"));
 app.set("view engine", 'ejs');
 
-// mongoose.connect("mongodb://localhost:27017/userDB",
-// {useNewUrlParser: true});
+console.log(process.env.API_KEY);
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
+const User = new mongoose.model("User", userSchema);
 
 //---------------------Port Section---------------------
 const PORT = process.env.PORT || 3000
@@ -24,10 +39,35 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/login", (req, res) => {
-  res.render("login");
-});
+app.route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
 
-app.get("/register", (req, res) => {
-  res.render("register");
-});
+    User.findOne({email: username}, (err, foundUser)=>{
+      if(err) console.log(err);
+      else {
+        if(foundUser){
+          if(foundUser.password === password) res.render("secrets");
+        }
+      }
+    });
+  });
+
+app.route("/register")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post((req, res) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password
+    });
+    newUser.save((err) => {
+      if (err) console.log(err);
+      else res.render("secrets");
+    });
+  });
